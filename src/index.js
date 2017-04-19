e128f1beef6dfd80b69a1540b91fde6cc8e7be5d
Michał Jezierski <ambinanitelo@gmail.com>
2017-04-19 11:28:09 +0200

    runner

--- src/index.js
+++ src/index.js
@@ -1,19 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
-// import { Provider } from 'react-redux';
-// import thunkMiddleware from 'redux-thunk'
-// import { createStore, applyMiddleware } from 'redux';
-// import reducer from './reducer';
+
 import List from './list';
 
-// const store = createStore(
-//     reducer,
-//     applyMiddleware(thunkMiddleware)
-// );
-
 render(
-    // <Provider store={ store }>
-        <List />,
-    // </Provider>,
+    <List />,
     document.getElementById('root')
 );
--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,7 @@ const path = require('path');
 
 module.exports = {
     entry: {
-        index: './index.js',
-        demo: './demo/app.js'
+        index: './src/index.js'
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
