129fe983cc0598fbf2e633f2d05efcbe555dc77d
Michał Jezierski <ambinanitelo@gmail.com>
2017-04-20 21:51:00 +0200

    loading (p)css files in tests - a naive way

--- jsdom-setup.js
+++ jsdom-setup.js
@@ -1,5 +1,9 @@
+const hook = require('node-hook').hook;
 const jsdom = require('jsdom');
 
 global.document = jsdom.jsdom('<body></body>');
 global.window = document.defaultView;
 global.navigator = window.navigator;
+
+hook('.css', () => '');
+hook('.pcss', () => '');
