9aabc93a9cbd6251cb67aa6cef2ebc68ef68fd29
Michał Jezierski <ambinanitelo@gmail.com>
2017-04-20 20:32:55 +0200

    reset-css imported before other components' styles

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import React from 'react';
 import { render } from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import thunkMiddleware from 'redux-thunk';
+
+// this needs to be imported before any other app components
+import 'reset-css/reset.css';
+
 import reducers from './reducers/reducers';
 import Foobar from './components/foobar';
-import thunkMiddleware from 'redux-thunk';
 
 const container = document.getElementById('tfs4jira__foobar');
 const data = JSON.parse(container.getAttribute('data-initial'));
